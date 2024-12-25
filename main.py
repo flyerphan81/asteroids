12cbaacfcfdebbf7ee9395707566c0a2218b3dd4
flyerphan81 <jrgardner1981@gmail.com>
2024-12-25 15:29:33 -0500

    Added player drawing and spawn

--- main.py
+++ main.py
@@ -3,14 +3,13 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 def main():
-    #print("Starting asteroids!")
-    #print(f"Screen width: {SCREEN_WIDTH}")
-    #print(f"Screen height: {SCREEN_HEIGHT}")
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    player1 = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
+
     clock = pygame.time.Clock()
     dt = 0
     
@@ -19,6 +18,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player1.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000 
 
